71fd2bc819c9d26d7d00cfc90088da14e7b2a6db
ajmiller <ajmiller611@live.com>
2024-10-31 14:07:03 -0400

    initilization spring boot app

---
+++ src/main/java/com/example/military_logistics_mgmt/MilitaryLogisticsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.military_logistics_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilitaryLogisticsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilitaryLogisticsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/military_logistics_mgmt/MilitaryLogisticsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.military_logistics_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitaryLogisticsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
